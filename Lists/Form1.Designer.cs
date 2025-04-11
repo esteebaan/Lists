5ead90a746814037bac780c0ba80fe67d6ef3aad
USER <USER@DESKTOP-622UG1M>
2025-04-11 10:19:36 -0500

    Add project files.

---
+++ Lists/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Lists
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtElemento = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lstElementos = new System.Windows.Forms.ListBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtElemento
+            // 
+            this.txtElemento.Location = new System.Drawing.Point(127, 34);
+            this.txtElemento.Name = "txtElemento";
+            this.txtElemento.Size = new System.Drawing.Size(100, 22);
+            this.txtElemento.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(274, 32);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lstElementos
+            // 
+            this.lstElementos.FormattingEnabled = true;
+            this.lstElementos.ItemHeight = 16;
+            this.lstElementos.Location = new System.Drawing.Point(127, 105);
+            this.lstElementos.Name = "lstElementos";
+            this.lstElementos.Size = new System.Drawing.Size(120, 84);
+            this.lstElementos.TabIndex = 2;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(299, 132);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.lstElementos);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtElemento);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtElemento;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListBox lstElementos;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ Lists/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lists
+{
+    public partial class Form1: Form
+    {
+        // Lista para almacenar elementos 
+
+        private List<string> elementos = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string nuevoElemento = txtElemento.Text;
+
+            if (!string.IsNullOrWhiteSpace(nuevoElemento))
+            {
+                elementos.Add(nuevoElemento);
+                ActualizarLista();
+                txtElemento.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingresa un elemento válido.");
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (lstElementos.SelectedItem != null)
+            {
+                elementos.Remove(lstElementos.SelectedItem.ToString());
+                ActualizarLista();
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un elemento para eliminar.");
+            }
+        }
+
+        // Método para actualizar ListBox 
+        private void ActualizarLista()
+        {
+            lstElementos.DataSource = null;
+            lstElementos.DataSource = elementos;
+        }
+    }
+}
